2c2e6a232cab9bbb69bce696df40e071ba6559ec
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-12-19 12:46:00 +1300

    Temporarily make the build pass

--- src/FirstAML.Couriers/Class1.cs
+++ src/FirstAML.Couriers/Class1.cs
@@ -2,6 +2,9 @@
 
 namespace FirstAML.Couriers
 {
+    /// <summary>
+    /// This documentation comment is here to make the build pass for now.
+    /// </summary>
     public class Class1
     {
     }
