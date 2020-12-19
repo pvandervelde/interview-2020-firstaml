dee4152c9f7a2739cd720cc3c12c1f5654553aa6
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-12-19 15:30:17 +1300

    Adding a weight property to a parcel so that we can charge based on weight too.

--- src/FirstAML.Couriers.Tests/OrderIntegrationTest.cs
+++ src/FirstAML.Couriers.Tests/OrderIntegrationTest.cs
@@ -6,12 +6,12 @@ namespace FirstAML.Couriers.Tests
     public sealed class OrderIntegrationTest
     {
         [Test]
-        public void When_AnOrderIsCreated_WithTheSizeBasedCalculator_Then_TheTotalPrice_ShouldBeCorrect()
+        public void When_AnOrderIsCreated_WithSize_Then_TheTotalPrice_ShouldBeCorrect()
         {
-            var smallParcel = new Parcel(5, 6, 7);
-            var mediumParcel = new Parcel(10, 6, 7);
-            var largeParcel = new Parcel(50, 6, 7);
-            var extraLargeParcel = new Parcel(100, 6, 7);
+            var smallParcel = new Parcel(5, 6, 7, 0.5);
+            var mediumParcel = new Parcel(10, 6, 7, 1.5);
+            var largeParcel = new Parcel(50, 6, 7, 3.0);
+            var extraLargeParcel = new Parcel(100, 6, 7, 5.0);
 
             var calculator = new SizeBasedPriceCalculator();
             var order = new Order(calculator);
@@ -25,12 +25,31 @@ namespace FirstAML.Couriers.Tests
         }
 
         [Test]
-        public void When_AnOrderIsCreated_WithTheSizeBasedCalculator_And_SpeedyDelivery_Then_TheTotalPrice_ShouldBeCorrect()
+        public void When_AnOrderIsCreated_WithSizeAndWeight_Then_TheTotalPrice_ShouldBeCorrect()
         {
-            var smallParcel = new Parcel(5, 6, 7);
-            var mediumParcel = new Parcel(10, 6, 7);
-            var largeParcel = new Parcel(50, 6, 7);
-            var extraLargeParcel = new Parcel(100, 6, 7);
+            var smallParcel = new Parcel(5, 6, 7, 3.0);
+            var mediumParcel = new Parcel(10, 6, 7, 5.0);
+            var largeParcel = new Parcel(50, 6, 7, 8.0);
+            var extraLargeParcel = new Parcel(100, 6, 7, 12.0);
+
+            var calculator = new SizeBasedPriceCalculator();
+            var order = new Order(calculator);
+
+            order.Add(smallParcel);
+            order.Add(mediumParcel);
+            order.Add(largeParcel);
+            order.Add(extraLargeParcel);
+
+            Assert.That(order.TotalCost, Is.EqualTo(3m + 4m + 8m + 4m + 15m + 4m + 25m + 4m));
+        }
+
+        [Test]
+        public void When_AnOrderIsCreated_WithSize_And_SpeedyDelivery_Then_TheTotalPrice_ShouldBeCorrect()
+        {
+            var smallParcel = new Parcel(5, 6, 7, 0.5);
+            var mediumParcel = new Parcel(10, 6, 7, 1.5);
+            var largeParcel = new Parcel(50, 6, 7, 3.0);
+            var extraLargeParcel = new Parcel(100, 6, 7, 5.0);
 
             var calculator = new SizeBasedPriceCalculator();
             var order = new Order(calculator, speedyShipping:true);
@@ -41,6 +60,25 @@ namespace FirstAML.Couriers.Tests
             order.Add(extraLargeParcel);
 
             Assert.That(order.TotalCost, Is.EqualTo(2 * (3m + 8m + 15m + 25m)));
+        }
+
+        [Test]
+        public void When_AnOrderIsCreated_WithSizeAndWeight_And_SpeedyDelivery_Then_TheTotalPrice_ShouldBeCorrect()
+        {
+            var smallParcel = new Parcel(5, 6, 7, 3.0);
+            var mediumParcel = new Parcel(10, 6, 7, 5.0);
+            var largeParcel = new Parcel(50, 6, 7, 8.0);
+            var extraLargeParcel = new Parcel(100, 6, 7, 12.0);
+
+            var calculator = new SizeBasedPriceCalculator();
+            var order = new Order(calculator, speedyShipping: true);
+
+            order.Add(smallParcel);
+            order.Add(mediumParcel);
+            order.Add(largeParcel);
+            order.Add(extraLargeParcel);
+
+            Assert.That(order.TotalCost, Is.EqualTo(2 * (3m + 4m + 8m + 4m + 15m + 4m + 25m + 4m)));
         }
     }
 }
--- src/FirstAML.Couriers.Tests/ParcelTest.cs
+++ src/FirstAML.Couriers.Tests/ParcelTest.cs
@@ -12,31 +12,42 @@ namespace FirstAML.Couriers.Tests
             var width = 2;
             var height = 3;
 
-            var parcel = new Parcel(length, width, height);
+            var weight = 4.0;
+
+            var parcel = new Parcel(length, width, height, weight);
 
             Assert.That(parcel.LengthInCm, Is.EqualTo(length));
             Assert.That(parcel.WidthInCm, Is.EqualTo(width));
             Assert.That(parcel.HeightInCm, Is.EqualTo(height));
+
+            Assert.That(parcel.WeightInKg, Is.EqualTo(weight));
         }
 
         [Test]
         public void When_CreatingAPackage_WithNegativeHeight_Then_ExceptionIsThrown()
         {
-            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(1, 1, -1));
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(1, 1, -1, 1.0));
             Assert.That(exception.Message, Does.Contain("height"));
         }
 
         [Test]
         public void When_CreatingAPackage_WithNegativeLength_Then_ExceptionIsThrown()
         {
-            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(-1, 1, 1));
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(-1, 1, 1, 1.0));
             Assert.That(exception.Message, Does.Contain("length"));
+        }
+
+        [Test]
+        public void When_CreatingAPackage_WithNegativeWeight_Then_ExceptionIsThrown()
+        {
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(1, 1, 1, -1.0));
+            Assert.That(exception.Message, Does.Contain("weight"));
         }
 
         [Test]
         public void When_CreatingAPackage_WithNegativeWidth_Then_ExceptionIsThrown()
         {
-            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(1, -1, 1));
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(1, -1, 1, 1.0));
             Assert.That(exception.Message, Does.Contain("width"));
         }
     }
