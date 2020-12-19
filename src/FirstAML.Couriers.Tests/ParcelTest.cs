b6f390c058bb44e4d4dd582f9b2ffaf9a6649006
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-12-19 13:05:13 +1300

    Adding a class for parcels

---
+++ src/FirstAML.Couriers.Tests/ParcelTest.cs
@@ -0,0 +1,43 @@
+using System;
+using NUnit.Framework;
+
+namespace FirstAML.Couriers.Tests
+{
+    public sealed class Tests
+    {
+        [Test]
+        public void When_CreatingAPackage_Then_DimensionsShouldBeSet()
+        {
+            var length = 1;
+            var width = 2;
+            var height = 3;
+
+            var parcel = new Parcel(length, width, height);
+
+            Assert.AreEqual(length, parcel.LengthInCm);
+            Assert.AreEqual(width, parcel.WidthInCm);
+            Assert.AreEqual(height, parcel.HeightInCm);
+        }
+
+        [Test]
+        public void When_CreatingAPackage_WithNegativeHeight_Then_ExceptionIsThrown()
+        {
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(1, 1, -1));
+            Assert.That(exception.Message, Does.Contain("height"));
+        }
+
+        [Test]
+        public void When_CreatingAPackage_WithNegativeLength_Then_ExceptionIsThrown()
+        {
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(-1, 1, 1));
+            Assert.That(exception.Message, Does.Contain("length"));
+        }
+
+        [Test]
+        public void When_CreatingAPackage_WithNegativeWidth_Then_ExceptionIsThrown()
+        {
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() => new Parcel(1, -1, 1));
+            Assert.That(exception.Message, Does.Contain("width"));
+        }
+    }
+}
---
+++ src/FirstAML.Couriers/Parcel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace FirstAML.Couriers
+{
+    /// <summary>
+    /// This documentation comment is here to make the build pass for now.
+    /// </summary>
+    public sealed class Parcel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Parcel"/> class.
+        /// </summary>
+        /// <param name="lengthInCm">The length of the parcel in centimeters.</param>
+        /// <param name="widthInCm">The width of the parcel in centimeters.</param>
+        /// <param name="heightInCm">The height of the parcel in centimeters.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Thrown when one of the <paramref name="lengthInCm"/>, <paramref name="widthInCm"/> or
+        ///     <paramref name="heightInCm"/> is less than 0.
+        /// </exception>
+        public Parcel(int lengthInCm, int widthInCm, int heightInCm)
+        {
+            if (lengthInCm < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(lengthInCm),
+                    string.Format(
+                        "The length of the parcel should be greater than 0 cm. The supplied value was: {0}",
+                        lengthInCm));
+            }
+
+            if (widthInCm < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(widthInCm),
+                    string.Format(
+                        "The width of the parcel should be greater than 0 cm. The supplied value was: {0}",
+                        widthInCm));
+            }
+
+            if (heightInCm < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(heightInCm),
+                    string.Format(
+                        "The height of the parcel should be greater than 0 cm. The supplied value was: {0}",
+                        heightInCm));
+            }
+
+            LengthInCm = lengthInCm;
+            HeightInCm = heightInCm;
+            WidthInCm = widthInCm;
+        }
+
+        /// <summary>
+        /// Gets the height of the package in centimeters.
+        /// </summary>
+        public int HeightInCm
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the length of the package in centimeters.
+        /// </summary>
+        public int LengthInCm
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the width of the package in centimeters.
+        /// </summary>
+        public int WidthInCm
+        {
+            get;
+            private set;
+        }
+    }
+}
