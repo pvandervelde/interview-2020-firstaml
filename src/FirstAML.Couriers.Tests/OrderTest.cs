f949e7d15eae850128c36c07729df712ec190f8d
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-12-19 14:26:22 +1300

    Adding the Order type that stores information about an order

---
+++ src/FirstAML.Couriers.Tests/OrderTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+
+namespace FirstAML.Couriers.Tests
+{
+    [TestFixture]
+    public sealed class OrderTest
+    {
+        [Test]
+        public void When_AnOrderIsCreated_Then_TheTotalPrice_ShouldBeCorrect()
+        {
+            const decimal costPerParcel = 10m;
+
+            var calculator = new Mock<IPriceCalculator>();
+            {
+                calculator.Setup(c => c.Calculate(It.IsAny<Parcel>()))
+                    .Returns<Parcel>(p => new ParcelShippingInformation(p, costPerParcel, "not-an-actual-type"));
+            }
+            
+            var order = new Order(calculator.Object);
+
+            int count = 10;
+            for (int i = 0; i < count; i++)
+            {
+                var parcel = new Parcel(i + 1, i + 1, i + 1);
+                order.Add(parcel);
+            }
+
+            Assert.That(order.TotalCost, Is.EqualTo(count * costPerParcel));
+
+            Assert.That(order.Parcels.Count(), Is.EqualTo(count));
+            foreach (var parcel in order.Parcels)
+            {
+                Assert.That(parcel.Cost, Is.EqualTo(costPerParcel));
+            }
+        }
+
+        [Test]
+        public void When_AnOrderIsCreated_WithNullCalculator_Then_ExceptionIsThrown()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Order(null));
+        }
+    }
+}
---
+++ src/FirstAML.Couriers/Order.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstAML.Couriers
+{
+    /// <summary>
+    /// Stores information about an order of parcels and the costs associated with shipping the parcels.
+    /// </summary>
+    public sealed class Order : IOrder
+    {
+        private readonly List<ParcelShippingInformation> _parcels = new List<ParcelShippingInformation>();
+
+        private readonly IPriceCalculator _priceCalculator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Order"/> class.
+        /// </summary>
+        /// <param name="calculator">
+        ///     The <see cref="IPriceCalculator"/> instance that is used to calculate the shipping cost of
+        ///     an individual parcel.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if <paramref name="calculator"/> is <see langword="null" />.
+        /// </exception>
+        public Order(IPriceCalculator calculator)
+        {
+            _priceCalculator = calculator ?? throw new ArgumentNullException(nameof(calculator));
+        }
+
+        /// <summary>
+        /// Adds a new <see cref="Parcel"/> to the order.
+        /// </summary>
+        /// <param name="parcel">The parcel to add.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="parcel"/> is <see langword="null" />. </exception>
+        public void Add(Parcel parcel)
+        {
+            if (parcel is null)
+            {
+                throw new ArgumentNullException(nameof(parcel));
+            }
+
+            var shippingInformation = _priceCalculator.Calculate(parcel);
+            _parcels.Add(shippingInformation);
+
+            TotalCost += shippingInformation.Cost;
+        }
+
+        /// <summary>
+        /// Gets the collection of parcels and their shipping information.
+        /// </summary>
+        public IEnumerable<ParcelShippingInformation> Parcels
+        {
+            get
+            {
+                return _parcels.AsReadOnly();
+            }
+        }
+
+        /// <summary>
+        /// Gets the total cost for the current order.
+        /// </summary>
+        public decimal TotalCost { get; private set; } = 0;
+    }
+}
